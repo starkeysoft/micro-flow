6afa09c78945029ab8e8ace9ed095568970deb6d
Ron Roe <ronald.roe@gmail.com>
2026-03-22 22:01:06 +0000

    Checkpoint from Copilot CLI for coding agent session

--- src/classes/steps/index.js
+++ src/classes/steps/index.js
@@ -1,8 +1,8 @@
+export { default as Step } from './step.js';
+export { default as LogicStep } from './logic_step.js';
 export { default as Case } from './case.js';
 export { default as ConditionalStep } from './conditional_step.js';
 export { default as DelayStep } from './delay_step.js';
 export { default as FlowControlStep } from './flow_control_step.js';
-export { default as LogicStep } from './logic_step.js';
 export { default as LoopStep } from './loop_step.js';
-export { default as Step } from './step.js';
 export { default as SwitchStep } from './switch_step.js';
